e0cf72edf34bbec795aa4812148c927263b096df
z <z.y.j.1379@gmail.com>
2021-09-05 21:49:45 +0430

    change the threshold

--- edge_detection.py
+++ edge_detection.py
@@ -1,7 +1,7 @@
 import cv2
 
 # Read the original image
-img = cv2.imread('paper.jpeg')
+img = cv2.imread('paper_2.png')
 # Display original image
 # cv2.imshow('Original', img)
 # cv2.waitKey(0)
@@ -9,12 +9,12 @@ img = cv2.imread('paper.jpeg')
 # Convert to graycsale
 img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # Blur the image for better edge detection
-# img_blur = cv2.GaussianBlur(img_gray, (3,3), 0)
-img_blur = img_gray
+img_blur = cv2.GaussianBlur(img_gray, (3,3), 0)
+# img_blur = img_gray
 # Sobel Edge Detection
-sobelx = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=5) # Sobel Edge Detection on the X axis
-sobely = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=0, dy=1, ksize=5) # Sobel Edge Detection on the Y axis
-sobelxy = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=5) # Combined X and Y Sobel Edge Detection
+sobelx = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=1) # Sobel Edge Detection on the X axis
+sobely = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=0, dy=1, ksize=1) # Sobel Edge Detection on the Y axis
+sobelxy = cv2.Sobel(src=img_blur, ddepth=cv2.CV_64F, dx=1, dy=1, ksize=1) # Combined X and Y Sobel Edge Detection
 # Display Sobel Edge Detection Images
 # cv2.imshow('Sobel X', sobelx)
 # cv2.waitKey(0)
@@ -24,7 +24,7 @@ cv2.imshow('Sobel X Y using Sobel() function', sobelxy)
 cv2.waitKey(0)
 
 # Canny Edge Detection
-edges = cv2.Canny(image=img_blur, threshold1=100, threshold2=200) # Canny Edge Detection
+edges = cv2.Canny(image=img_blur, threshold1=0, threshold2=100) # Canny Edge Detection
 # Display Canny Edge Detection Image
 cv2.imshow('Canny Edge Detection', edges)
 cv2.waitKey(0)
